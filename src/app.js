109808c531474909234424af1a6f442e87045d90
Blondy Martinez <47917655+BlondyMartinez@users.noreply.github.com>
2024-02-01 14:14:26 +0000

    generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = [" ate", " peed", " crushed", " broke"];
+let what = [" my homework", " the keys", " the car"];
+let when = [
+  " before the class.",
+  " right on time.",
+  " when I finished.",
+  " during my lunch.",
+  " while I was praying."
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateExcuse() {
+  let excuse = "";
 
+  for (let i = 0; i < 4; i++) {
+    switch (i) {
+      case 0:
+        excuse = who[Math.floor(Math.random() * 4)];
+        break;
+      case 1:
+        excuse += action[Math.floor(Math.random() * 4)];
+        break;
+      case 2:
+        excuse += what[Math.floor(Math.random() * 3)];
+        break;
+      case 3:
+        excuse += when[Math.floor(Math.random() * 5)];
+        break;
+    }
+  }
+
+  document.getElementById("excuse").innerHTML = excuse;
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateExcuse();
 };
